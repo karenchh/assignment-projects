703fa00bee3242b19b03ad29a46057ac7714dd4d
karen chahrour <karenchahrour123@gmail.com>
2025-02-14 13:50:20 +0100

    Creating-ex3

---
+++ login.py
@@ -0,0 +1,11 @@
+# Login system 
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+correctpass = 1234
+
+if username == "admin" and password == "1234" :
+    print("Access granted. ")
+else : 
+    print("Access denied. ")
